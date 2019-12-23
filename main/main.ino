0193d5e1e930e5a84ff683d391c8b7e578da98c0
ELY M <elymbmx@gmail.com>
2019-12-23 10:05:16 -0600

    update hekate to 5.1.1

--- main/main.ino
+++ main/main.ino
@@ -13,7 +13,7 @@
 // Contains fuseeBin and FUSEE_BIN_LENGTH
 // Include only one payload here
 // Use tools/binConverter.py to convert any payload bin you wish to load
-#include "hekate_ctcaer_5.0.2.h"
+#include "hekate_ctcaer_5.1.1.h"
 
 #define INTERMEZZO_SIZE 92
 const byte intermezzo[INTERMEZZO_SIZE] =
